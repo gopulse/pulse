89899f99bf01cd24f1e6506a27945681febe4a9b
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-03-31 20:14:34 +0200

    define routing signs as constants

---
+++ constants/routingSigns.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	// WildcardSign is the sign for a wildcard route
+	WildcardSign = "*"
+
+	// ParamSign is the sign for a parameter
+	ParamSign = ":"
+
+	// OptionalSign is the sign for an optional parameter
+	OptionalSign = "?"
+)
