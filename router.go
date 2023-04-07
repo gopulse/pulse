3cba71afe5e280d17491fbc048e7dfe92bd17f37
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-04-08 01:04:45 +0200

    handle route parameters, fix empty handler bug

--- router.go
+++ router.go
@@ -2,57 +2,55 @@ package routing
 
 import (
 	"github.com/valyala/fasthttp"
+	"strings"
 )
 
 type Handler func(ctx *Context) error
 
-type Router struct {
-	routes          map[string]*route
-	stores          map[string]RouteStore
-	notFoundHandler Handler
-	maxParams       int
+type Route struct {
+	Path       string
+	ParamNames []string
+	Handlers   []Handler
 }
 
-func CombineHandlers(handlers ...Handler) Handler {
-	return func(ctx *Context) error {
-		for _, handler := range handlers {
-			if err := handler(ctx); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
+type Router struct {
+	routes          map[string][]*Route
+	notFoundHandler Handler
 }
 
 func New() *Router {
 	return &Router{
-		routes: make(map[string]*route),
-		stores: make(map[string]RouteStore),
+		routes: make(map[string][]*Route),
 	}
 }
 
 func (r *Router) add(method, path string, handlers []Handler) {
-	s := r.stores[method]
-	if s == nil {
-		s = newStore()
-		r.stores[method] = s
+	route := &Route{
+		Path:     path,
+		Handlers: handlers,
 	}
-	if n := s.Add(path, handlers); n > r.maxParams {
-		r.maxParams = n
+
+	parts := strings.Split(path, "/")
+	for _, part := range parts {
+		if strings.HasPrefix(part, ":") {
+			route.ParamNames = append(route.ParamNames, strings.TrimPrefix(part, ":"))
+		}
 	}
-	r.routes[method+path] = &route{method, path, CombineHandlers(handlers...)}
+	route.Path = strings.Join(parts, "/")
+
+	r.routes[method] = append(r.routes[method], route)
 }
 
 func (r *Router) find(method, path string) []Handler {
-	s := r.stores[method]
-	if s == nil {
-		return []Handler{}
+	routes := r.routes[method]
+	for _, route := range routes {
+		if matches, params := route.match(path); matches {
+			c := NewContext(nil, nil)
+			c.params = params
+			return route.Handlers
+		}
 	}
-	handlers, ok := s.Get(path).([]Handler)
-	if !ok {
-		return []Handler{}
-	}
-	return handlers
+	return nil
 }
 
 func RouterHandler(router *Router) func(ctx *fasthttp.RequestCtx) {
@@ -64,7 +62,24 @@ func RouterHandler(router *Router) func(ctx *fasthttp.RequestCtx) {
 			ctx.Error("Not found", fasthttp.StatusNotFound)
 			return
 		}
-		c := NewContext(ctx)
+
+		params := make(map[string]string)
+		for _, route := range router.routes[method] {
+			if matches, params := route.match(path); matches {
+				c := NewContext(ctx, nil).WithParams(params)
+				for _, h := range handlers {
+					err := h(c)
+					if err != nil {
+						ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+						return
+					}
+				}
+				return
+			}
+		}
+
+		c := NewContext(ctx, params)
+
 		for _, h := range handlers {
 			err := h(c)
 			if err != nil {
@@ -73,4 +88,25 @@ func RouterHandler(router *Router) func(ctx *fasthttp.RequestCtx) {
 			}
 		}
 	}
+}
+
+func (r *Route) match(path string) (bool, map[string]string) {
+	parts := strings.Split(path, "/")
+	routeParts := strings.Split(r.Path, "/")
+
+	if len(parts) != len(routeParts) {
+		return false, nil
+	}
+
+	params := make(map[string]string)
+	for i, part := range routeParts {
+		if strings.HasPrefix(part, ":") {
+			paramName := strings.TrimPrefix(part, ":")
+			params[paramName] = parts[i]
+		} else if part != parts[i] {
+			return false, nil
+		}
+	}
+
+	return true, params
 }
