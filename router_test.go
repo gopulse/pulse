1868d7cbcd2112d287bfc30a763e764f231cfc50
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-04-09 02:46:32 +0200

    arrange test file

--- router_test.go
+++ router_test.go
@@ -1,8 +1,10 @@
 package routing
 
 import (
+	"fmt"
 	"github.com/valyala/fasthttp"
 	"testing"
+	"time"
 )
 
 func TestRouterHandler(t *testing.T) {
@@ -15,7 +17,76 @@ func TestRouterHandler(t *testing.T) {
 		return nil
 	})
 
-	router.Use("POST", CORSMiddleware())
+	err := fasthttp.ListenAndServe(":8083", RouterHandler(router))
+	if err != nil {
+		return
+	}
+}
+
+func TestCORSMiddleware(t *testing.T) {
+	router := New()
+
+	router.Get("/", func(ctx *Context) error {
+		return nil
+	})
+
+	router.Use("GET", CORSMiddleware())
+
+	err := fasthttp.ListenAndServe(":8083", RouterHandler(router))
+	if err != nil {
+		return
+	}
+}
+
+func TestContext_SetCookie(t *testing.T) {
+	router := New()
+
+	router.Get("/", func(ctx *Context) error {
+		cookie := Cookie{
+			Name:        "Test Cookie 1",
+			Value:       "Test Cookie 1",
+			Path:        "/",
+			Domain:      "localhost",
+			MaxAge:      0,
+			Expires:     time.Now().Add(24 * time.Hour),
+			Secure:      false,
+			HTTPOnly:    false,
+			SameSite:    "Lax",
+			SessionOnly: false,
+		}
+		ctx.SetCookie(&cookie)
+		return nil
+	})
+
+	err := fasthttp.ListenAndServe(":8083", RouterHandler(router))
+	if err != nil {
+		return
+	}
+}
+
+func TestContext_GetCookie(t *testing.T) {
+	router := New()
+
+	router.Get("/", func(ctx *Context) error {
+		cookie := ctx.GetCookie("test")
+		ctx.String(cookie)
+		return nil
+	})
+
+	err := fasthttp.ListenAndServe(":8083", RouterHandler(router))
+	if err != nil {
+		return
+	}
+}
+
+func TestContext_SetHeader(t *testing.T) {
+	router := New()
+
+	router.Get("/", func(ctx *Context) error {
+		ctx.SetHeader("Test Header", "test header value")
+		fmt.Println(ctx.GetHeader("test"))
+		return nil
+	})
 
 	err := fasthttp.ListenAndServe(":8083", RouterHandler(router))
 	if err != nil {
