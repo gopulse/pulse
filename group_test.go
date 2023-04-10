c04ec74bd8da1b519388487faa0a363ad577b2fb
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-04-11 01:01:02 +0200

    route group test

---
+++ group_test.go
@@ -0,0 +1,20 @@
+package pulse
+
+import "testing"
+
+func TestRouter_Group(t *testing.T) {
+	router := NewRouter()
+	api := &Group{
+		prefix: "/api",
+		router: router,
+	}
+	v1 := api.Group("/v1")
+	v1.GET("/users", func(ctx *Context) error {
+		ctx.String("users")
+		return nil
+	})
+
+	app.Router = router
+
+	app.Run(":8083")
+}
