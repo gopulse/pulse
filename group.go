8e8c556c9abf17a9e370455148c23ee4f14c2f36
Ahmed Mohamed <ahmed_noreldin@outlook.com>
2023-04-13 14:11:11 +0200

    export group struct fields (#7)

--- group.go
+++ group.go
@@ -3,50 +3,50 @@ package pulse
 import "fmt"
 
 type Group struct {
-	prefix string
-	router *Router
+	Prefix string
+	Router *Router
 }
 
 func (g *Group) Group(prefix string) *Group {
 	return &Group{
-		prefix: g.prefix + prefix,
-		router: g.router,
+		Prefix: g.Prefix + prefix,
+		Router: g.Router,
 	}
 }
 
 func (g *Group) Use(middleware Middleware) {
-	g.router.Use(g.prefix, middleware)
+	g.Router.Use(g.Prefix, middleware)
 }
 
 func (g *Group) GET(path string, handlers ...Handler) {
-	fmt.Println(g.prefix + path)
-	g.router.Get(g.prefix+path, handlers...)
+	fmt.Println(g.Prefix + path)
+	g.Router.Get(g.Prefix+path, handlers...)
 }
 
 func (g *Group) POST(path string, handlers ...Handler) {
-	g.router.Post(g.prefix+path, handlers...)
+	g.Router.Post(g.Prefix+path, handlers...)
 }
 
 func (g *Group) PUT(path string, handlers ...Handler) {
-	g.router.Put(g.prefix+path, handlers...)
+	g.Router.Put(g.Prefix+path, handlers...)
 }
 
 func (g *Group) DELETE(path string, handlers ...Handler) {
-	g.router.Delete(g.prefix+path, handlers...)
+	g.Router.Delete(g.Prefix+path, handlers...)
 }
 
 func (g *Group) PATCH(path string, handlers ...Handler) {
-	g.router.Patch(g.prefix+path, handlers...)
+	g.Router.Patch(g.Prefix+path, handlers...)
 }
 
 func (g *Group) OPTIONS(path string, handlers ...Handler) {
-	g.router.Options(g.prefix+path, handlers...)
+	g.Router.Options(g.Prefix+path, handlers...)
 }
 
 func (g *Group) HEAD(path string, handlers ...Handler) {
-	g.router.Head(g.prefix+path, handlers...)
+	g.Router.Head(g.Prefix+path, handlers...)
 }
 
 func (g *Group) Static(path, root string, config *Static) {
-	g.router.Static(g.prefix+path, root, config)
+	g.Router.Static(g.Prefix+path, root, config)
 }
