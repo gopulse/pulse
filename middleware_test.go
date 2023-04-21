b8efe09ab6e9bae175edc123c754b0899948fe8b
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-04-21 23:54:14 +0200

    cover CORSMiddleware method

--- middleware_test.go
+++ middleware_test.go
@@ -31,3 +31,47 @@ func TestRouter_Use(t *testing.T) {
 		t.Errorf("unexpected status code: %d", rec.Code)
 	}
 }
+
+func TestCORSMiddleware(t *testing.T) {
+	// create a test context
+	req := httptest.NewRequest("GET", "/test", nil)
+	w := httptest.NewRecorder()
+	ctx := NewContext(w, req)
+
+	// create a mock handler
+	mockHandler := func(ctx *Context) error {
+		return nil
+	}
+
+	// create the CORS middleware
+	corsMiddleware := CORSMiddleware()
+
+	// wrap the mock handler with the CORS middleware
+	handler := corsMiddleware(mockHandler)
+
+	// call the handler with the test context
+	err := handler(ctx)
+
+	// get the http.Response from the ResponseRecorder using Result()
+	res := w.Result()
+
+	// check if the Access-Control-Allow-Origin header was set to "*"
+	if header := res.Header.Get("Access-Control-Allow-Origin"); header != "*" {
+		t.Errorf("Expected Access-Control-Allow-Origin header to be \"*\", but got %q", header)
+	}
+
+	// check if the Access-Control-Allow-Methods header was set correctly
+	if header := res.Header.Get("Access-Control-Allow-Methods"); header != "POST, GET, OPTIONS, PUT, DELETE" {
+		t.Errorf("Expected Access-Control-Allow-Methods header to be \"POST, GET, OPTIONS, PUT, DELETE\", but got %q", header)
+	}
+
+	// check if the Access-Control-Allow-Headers header was set correctly
+	if header := res.Header.Get("Access-Control-Allow-Headers"); header != "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization" {
+		t.Errorf("Expected Access-Control-Allow-Headers header to be \"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization\", but got %q", header)
+	}
+
+	// check if the handler returned no error
+	if err != nil {
+		t.Errorf("Expected handler to return no error, but got %v", err)
+	}
+}
